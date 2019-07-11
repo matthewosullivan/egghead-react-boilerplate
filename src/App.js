26a2df30600afc3807379ab9a39a9174e599a83f
mosullivan <mosullivan@tripadvisor.com>
2019-07-11 21:02:29 +1000

    updates

--- src/App.js
+++ src/App.js
@@ -3,37 +3,35 @@ import { hot } from 'react-hot-loader'
 
 const Warning = React.lazy(() => import('./Warning'))
 
-const a = 'A'
-
 class App extends React.Component {
-    state = {
-        count: 0
-    }
+  state = {
+    count: 0
+  }
 
-    increment = () => {
-        this.setState(state => ({ count: state.count + 1 }))
-    }
+  increment = () => {
+    this.setState(state => ({ count: state.count + 1 }))
+  }
 
-    decrement = () => {
-        this.setState(state => ({ count: state.count - 1 }))
-    }
+  decrement = () => {
+    this.setState(state => ({ count: state.count - 1 }))
+  }
 
-    render() {
-        const { count } = this.state
-        return (
-            <div>
-                <h1>Hello World!</h1>
-                <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
-                <button onClick={this.increment}>+</button>
-                <button onClick={this.decrement}>-</button>
-                {count > 10 ? (
-                    <React.Suspense fallback={null}>
-                        <Warning />
-                    </React.Suspense>
-                ) : null}
-            </div>
-        )
-    }
+  render() {
+    const { count } = this.state
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
+      </div>
+    )
+  }
 }
 
 export default hot(module)(App)
